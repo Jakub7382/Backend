e9c1d611f54dc03994df07002f985b684ab230e1
Jakub Rolbiecki <jakub7382@gamil.com>
2023-12-09 21:04:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## How to run the project locally
+
+1. Navigate to the project directory:
+
+   ```
+   cd backend
+   ```
+
+2. Install dependencies:
+
+   ```
+   npm install
+   ```
+
+3. Run the project:
+
+   ```
+   npm start
+   ```
+
---
+++ api/routes/books.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const checkAuth = require("../middleware/checkAuth");
+const router = express.Router();
+const BookController = require("../controllers/books");
+
+router.get("/", BookController.getAllBooks);
+
+router.post("/", checkAuth, BookController.addBook);
+
+router.put("/:id", checkAuth, BookController.updateBook);
+
+router.delete("/:id", checkAuth, BookController.deleteBook);
+
+router.get("/:id", BookController.getBook);
+
+module.exports = router;
